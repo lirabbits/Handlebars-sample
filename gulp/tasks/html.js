c65e57a297c1a1d682cc00a8a2557dcebd3b68e4
noramega <noramageca@gmail.com>
2019-04-07 02:40:32 +0900

    add: watch, defaultタスクを追加

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,9 +1,15 @@
 import gulp from 'gulp';
+import changed from 'gulp-changed';
+
 import path from '../config.js';
 
 const htmlTask = () => {
     const {src, dest} = path.html;
-    return gulp.src(src).pipe(gulp.dest(dest));
+    return (
+        gulp.src(src)
+        .pipe(changed(dest))
+        .pipe(gulp.dest(dest))
+    );
 };
 
 export default htmlTask;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,22 @@
-import {task} from 'gulp';
+import { task, watch, parallel} from 'gulp';
 import _ from 'lodash';
 
 import html from './gulp/tasks/html.js';
+import path from "./gulp/config";
 
-_.forEach({html}, (val, key) => task(key, val));
+/** タスク一覧
+ *  タスク名: タスク内容
+ */
+const tasks = {html};
+
+_.forEach(tasks, (val, key) => task(key, val));
+
+/**
+ *  ファイル監視
+ */
+task('watch', () => {
+    const task = _.map(tasks, (val, key) => key);
+    return watch(path[task].src, parallel([task]));
+});
+
+task('default', parallel(_.map(tasks, (val, key) => key), 'watch'));
