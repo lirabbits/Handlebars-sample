e1dd3cebda1c61d6992983c2faf0cd4fe3dbc1e8
noramega <noramageca@gmail.com>
2019-04-07 20:09:48 +0900

    add: browser-sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,9 @@
-import { task, watch, parallel} from 'gulp';
+import { task, watch, parallel, series} from 'gulp';
 import _ from 'lodash';
 
 import html from './gulp/tasks/html.js';
 import path from "./gulp/config";
+import bs from "browser-sync";
 
 /** タスク一覧
  *  タスク名: タスク内容
@@ -12,11 +13,20 @@ const tasks = {html};
 _.forEach(tasks, (val, key) => task(key, val));
 
 /**
+ * broserSync
+ */
+task('bs', () => bs({server: './dest'}));
+task('reload', done => {
+    bs.reload();
+    done();
+});
+
+/**
  *  ファイル監視
  */
 task('watch', () => {
     const task = _.map(tasks, (val, key) => key);
-    return watch(path[task].src, parallel([task]));
+    return watch(path[task].src, series(parallel([task]), 'reload'));
 });
 
-task('default', parallel(_.map(tasks, (val, key) => key), 'watch'));
+task('default', parallel(_.map(tasks, (val, key) => key), 'watch', 'bs'));
